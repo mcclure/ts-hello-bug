d34af292cbd6d57d83ca048005eee68a908a12c4
mcc <andi.m.mcclure@gmail.com>
2019-12-09 23:23:32 -0500

    Typescript editor demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,3 @@
-import { h, render, Component } from "preact";
+import { demoFunction } from "./dependencyOne"
 
-declare let require:any
-
-let parentNode = document.getElementById("content")
-let replaceNode = document.getElementById("initial-loading")
-
-class Content extends Component<any, any> {
-  constructor(props:{}) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>Hello</div>
-    )
-  }
-}
-
-render(
-  <Content />,
-  parentNode, replaceNode
-);
+let x = demoFunction()
