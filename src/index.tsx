45dda8f7cf8cc141bbc8e2cb84e8fc2f996ed288
mcc <andi.m.mcclure@gmail.com>
2019-12-10 01:35:27 -0500

    Cursed index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,66 @@
-import { h, render, Component } from "preact";
+import { h, render, Context, createContext, JSX } from "preact";
+import { useContext } from "preact/hooks";
+import { OrderedSet } from "immutable"
 
 declare let require:any
+
+let verbose = false
+
+// ---- Helpers ----
+
+class Refresher {
+  private refreshed = true
+  public refresh:()=>void
+  constructor(refreshCallback :()=>void, dontRenderYet?:boolean) {
+    this.refresh = () => {
+      this.refreshed = true
+      refreshCallback()
+    }
+    if (!dontRenderYet)
+      this.refresh()
+  }
+  request() {
+     if (this.refreshed) {
+      this.refreshed = false
+      requestAnimationFrame(this.refresh)
+    }
+  }
+}
+
+class State<T> {
+  public context: Context<T>
+  constructor(public value:T) {
+    this.context = createContext(value)
+  }
+}
+
+class StateGroup {
+  constructor(public states:State<any>[]) {}
+  render(inside:JSX.Element) {
+    for (let X of this.states) {
+      inside = <X.context.Provider value={X.value}>{inside}</X.context.Provider>
+    }
+    return inside
+  }
+}
+
+// ----- Display -----
 
 let parentNode = document.getElementById("content")
 let replaceNode = document.getElementById("initial-loading")
 
-class Content extends Component<any, any> {
-  constructor(props:{}) {
-    super(props);
-    this.state = {};
-  }
+let SelfId = new State("")
+let PreconnectList = new State(OrderedSet<string>())
+let ConnectList = new State(OrderedSet<string>())
 
-  render() {
-    return (
-      <div>Hello</div>
-    )
-  }
+let states = new StateGroup([SelfId, PreconnectList, ConnectList])
+
+function UserBox() {
+  const selfId = useContext(SelfId.context)
+  return <div>You are {selfId ? selfId : "[pending]"}</div>
 }
 
-render(
-  <Content />,
-  parentNode, replaceNode
-);
+// HERE IS THE LINE WITH THE CURSE
+function UsersBox(props: {list: State<OrderedSet<string>>>, label:string}) {
+
+}
