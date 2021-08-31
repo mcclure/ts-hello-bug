0f1c8bdf107c285f21f221a71ac66a2da26df2cd
mcc <andi.m.mcclure@gmail.com>
2021-08-30 22:23:03 -0400

    Commander support

--- src/app.ts
+++ src/app.ts
@@ -5,4 +5,21 @@
 import sourceMapSupport from 'source-map-support'
 sourceMapSupport.install()
 
-console.log("App running")
+import { Command } from 'commander';
+const program = new Command();
+
+program
+	.version("0.0.1")
+	.option('-o, --option <word>', 'Example option')
+	.option('-f, --flag', 'Example flag')
+	.arguments("<file>")
+	.parse()
+
+if (program.args.length < 1)
+	console.error("No file specified")
+if (program.args.length > 1)
+	console.error("Too many files specified, at the moment the limit is 1")
+
+const opts = program.opts()
+
+console.log(`File ${program.args[0]}${opts.option || opts.flag ? " with ":""}${opts.flag?"flag":""}${opts.option && opts.flag ? ", ": ""}${opts.option || ""}`)
