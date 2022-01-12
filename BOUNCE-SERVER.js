19ee30a49d2e029f2e9fab80b78df37827dea13c
mcc <andi.m.mcclure@gmail.com>
2022-01-12 13:11:31 -0500

    Don't put hostname on public commit

--- BOUNCE-SERVER.js
+++ BOUNCE-SERVER.js
@@ -1,4 +1,4 @@
-const hostname = "DATA.RUNHELLO.COM"
+const hostname = "INSERT-HOSTNAME-HERE.COM"
 const publicKey = "12D3KooWGwQNDjSj8yww4GRg75ery7SPqgz9gAyh32rxHVBBMnHo"
 
 module.exports = {hostname, publicKey}
